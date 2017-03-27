dd2723eb3e9f319c9001e6b738687ec22fd6e2cd
MariusLuding <mluding@gmail.com>
2017-03-27 19:54:56 +0200

    Beschreibungen hinzugefügt.

--- matze/src/com/mymart/CartItem.java
+++ matze/src/com/mymart/CartItem.java
@@ -7,17 +7,29 @@ public class CartItem {
     private String name;
     private int quantity;
     private double pricePerUnit;
-
+    /**
+     * initialisiert einen einzukaufenden Artikel mit den gegebenen Daten
+     * @param name Name des Artikels
+     * @param quantity Vorhandene Anzahl des Artikels
+     * @param pricePerUnit Preis pro Artikel
+     */
     public CartItem(String name, int quantity, double pricePerUnit) {
         setQuantity(quantity);
         this.name = name;
         this.pricePerUnit = pricePerUnit;
     }
-
+    /**
+     *
+     * @return Liefert den Gesamtpreis des Artikels.
+     * D. h. bei z. B. vier Dosen Hundefutterzu je 3,20 liefert diese Methode 12,80.
+     */
     public double getCost() {
         return (double) quantity*pricePerUnit;
     }
-
+    /**
+     *  Setzt die Anzahl auf den gegebenen Wert.
+     * @param quantity
+     */
     public void setQuantity(int quantity) {
         if(quantity >= 1) {
             this.quantity = quantity;
