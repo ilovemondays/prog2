84d75eb13a617a901074055af03fa124597b2695
ColdStormy <jannistb@gmail.com>
2017-03-27 20:22:59 +0200

    Beschreibung hinzugefügt.

--- jtb/src/com/mymart/CartItem.java
+++ jtb/src/com/mymart/CartItem.java
@@ -1,14 +1,22 @@
 package com.mymart;
 
-import java.util.Formatter;
 import java.util.Locale;
 
+/**
+ * An item to be added to a shoppingcart
+ */
 public class CartItem {
 
     private String name;
     private int quantity;
     private double pricePerUnit;
 
+    /**
+     *
+     * @param name
+     * @param quantity int The amount of ordered items for this article.
+     * @param pricePerUnit double Price per unit.
+     */
     public CartItem(String name, int quantity, double pricePerUnit) {
 
         this.name = name;
@@ -16,16 +24,28 @@ public class CartItem {
         this.pricePerUnit = pricePerUnit;
     }
 
+    /**
+     *
+     * @return double Get the total costs.
+     */
     public double getCost() {
         return quantity*pricePerUnit;
     }
 
+    /**
+     * Set an amount of items.
+     * @param q int Must be higher or equal 1.
+     */
     public void setQuantity(int q) {
         if(q<1)
             throw new IllegalArgumentException("Below 1");
         quantity = q;
     }
 
+    /**
+     *
+     * @return String "Quantity x name   Price per Unit  Total costs"
+     */
     public String toString() {
         return String.format(Locale.GERMANY, "%3d x %30s   %10.2f  %10.2f", quantity, name, pricePerUnit, pricePerUnit*quantity );
     }
--- jtb/src/com/mymart/ShoppingCart.java
+++ jtb/src/com/mymart/ShoppingCart.java
@@ -3,18 +3,32 @@ package com.mymart;
 import java.util.ArrayList;
 import java.util.Locale;
 
+/**
+ * a Shoppingcart, contains a list of CartItems.
+ */
 public class ShoppingCart {
 
     private static ArrayList<CartItem> items;
 
+    /**
+     * Generate a shopping cart.
+     */
     public ShoppingCart() {
         items = new ArrayList<>();
     }
 
+    /**
+     * Add an item to the shopping cart.
+     * @param item CartItem
+     */
     public static void addItem(CartItem item) {
         items.add(item);
     }
 
+    /**
+     *
+     * @return double Total costs.
+     */
     public static double getTotalCost() {
         double sum = 0;
 
@@ -25,6 +39,10 @@ public class ShoppingCart {
         return sum;
     }
 
+    /**
+     *
+     * @return String A list of each items attributes (see CartItem) plus the total costs of all this shoppin cart items.
+     */
     public String toString() {
 
         StringBuilder sb = new StringBuilder();
