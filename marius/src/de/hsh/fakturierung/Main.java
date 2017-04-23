0bded9e0aa3342b589e7f43ecec509688913023a
MariusLuding <mluding@gmail.com>
2017-04-23 17:54:18 +0200

    Main angepasst

--- marius/src/de/hsh/fakturierung/Main.java
+++ marius/src/de/hsh/fakturierung/Main.java
@@ -6,11 +6,13 @@ import de.hsh.fakturierung.rechnung.Rechnung;
  * Created by Marius Luding on 23.04.2017 for group 13
  */
 public class Main {
-    public static void Main(String[] args) {
+
+    public static void main(String[] args) {
         Rechnung r  = new Rechnung( 1337);
         r.addPos(58008,9.99);
         r.addPos(12321, 127.00);
-        System.out.println(r.getPreis(50800));
+        System.out.println(r.getArtikelnummer(0)+ ":" + r.getPreis(0));
+        System.out.println(r.getArtikelnummer(1)+ ":" + r.getPreis(1));
 
     }
 }
