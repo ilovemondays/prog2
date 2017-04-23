747e5cbd7e05253559be9d5ce92333f5ee7e5c85
Matthias <mat.dietrich1985@gmail.com>
2017-04-23 17:36:22 +0200

    adding Aufgabe 11 (noch nicht 100% Graja-approved)

---
+++ matze/src/u11/Main.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Team 13 on 23.04.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Ballspiel s= new Ballspiel();
+        System.out.println("Ballspiel: ");
+        System.out.println("  Mannschaften: " + s.getMannschaften());
+        System.out.println("  Spieldauer  : " + s.getSpieldauer());
+        System.out.println("  Bälle       : " + s.getBaelle());
+        Fussball f= new Fussball();
+        System.out.println("Fussball: ");
+        System.out.println("  Mannschaften: " + f.getMannschaften());
+        System.out.println("  Spieldauer  : " + f.getSpieldauer());
+        System.out.println("  Bälle       : " + f.getBaelle());
+        System.out.println("  Einwurf     : " + f.einwurfRegel());
+        System.out.println("  Ecke        : " + f.eckRegel());
+        Handball h= new Handball();
+        System.out.println("Handball: ");
+        System.out.println("  Mannschaften: " + h.getMannschaften());
+        System.out.println("  Spieldauer  : " + h.getSpieldauer());
+        System.out.println("  Bälle       : " + h.getBaelle());
+        System.out.println("  Mannsch.gr. : " + h.mannschaftsGroesse());
+        Jonglieren j = new Jonglieren();
+        System.out.println("Jonglieren: ");
+        System.out.println("  Mannschaften: " + j.getMannschaften());
+        System.out.println("  Spieldauer  : " + j.getSpieldauer());
+        System.out.println("  Bälle       : " + j.getBaelle());
+        System.out.println("  Lernen      : " + j.lernRegel());
+        BeachSoccer b= new BeachSoccer();
+        System.out.println("BeachSoccer: ");
+        System.out.println("  Mannschaften: " + b.getMannschaften());
+        System.out.println("  Spieldauer  : " + b.getSpieldauer());
+        System.out.println("  Bälle       : " + b.getBaelle());
+        System.out.println("  Einwurf     : " + b.einwurfRegel());
+        System.out.println("  Ecke        : " + b.eckRegel());
+    }
+}
