7bc4ac56f03386241b2722870bd2087e5cd707bd
MariusLuding <mluding@gmail.com>
2017-05-13 18:48:34 +0200

    U08 angelegt

---
+++ marius/src/de/hsh/prog/werkzeuge/Main.java
@@ -0,0 +1,14 @@
+package de.hsh.prog.werkzeuge;
+
+/**
+ * Also farily useless
+ * @author Grp13
+ * @version 0.1
+ * @since 2017-05-13
+ */
+public class Main {
+    public static void main(String[] args) {
+        SomeClass s= new SomeClass();
+        System.out.println(s);
+    }
+}
---
+++ marius/src/de/hsh/prog/werkzeuge/SomeClass.java
@@ -0,0 +1,15 @@
+package de.hsh.prog.werkzeuge;
+
+/**
+ * A class with hardly any use
+ */
+public class SomeClass {
+
+    /**
+     * Standard Utility method
+     * @return "this is "+ Name of clsas
+     */
+    @Override public String toString() {
+        return "This is SomeClass";     //hier könnte ihre Werbung stehen
+    }
+}
