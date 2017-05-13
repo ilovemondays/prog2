ef5e177528f255f8858900777e638f98b812f18c
Matthias Dietrich <matthias.dietrich@stud.hs-hannover.de>
2017-05-13 18:23:23 +0200

    adding files for u16

---
+++ matze/src/de/hsh/prog/werkzeuge/Main.java
@@ -0,0 +1,17 @@
+package de.hsh.prog.werkzeuge;
+
+/**
+ * Hier befindet sich das Hauptprogramm, kühl und schattig lagernd.
+ * @author team 13
+ */
+public class Main {
+
+    /**
+     * Aufruf und Ausgabe von SomeClass
+     * @param args optionale string parameter, werden von uns ignoriert ;)
+     */
+    public static void main(String[] args) {
+        SomeClass s= new SomeClass();
+        System.out.println(s);
+    }
+}
---
+++ matze/src/de/hsh/prog/werkzeuge/SomeClass.java
@@ -0,0 +1,16 @@
+package de.hsh.prog.werkzeuge;
+
+/**
+ * SomeCLass mach some stuff
+ * @author Team 13
+ */
+public class SomeClass {
+
+    /**
+     * Überschreibt die standard toString Methode und gibt Poesie zurück.
+     * @return String
+     */
+    @Override public String toString() {
+        return "This is SomeClass";
+    }
+}
